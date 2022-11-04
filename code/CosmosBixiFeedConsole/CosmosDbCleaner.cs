e5f9809b226168db5975a47bc86a145178f7a120
vplauzon <vincent_lauzon@yahoo.com.au>
2022-11-04 17:06:17 -0400

    Clean

--- code/CosmosBixiFeedConsole/CosmosDbCleaner.cs
+++ code/CosmosBixiFeedConsole/CosmosDbCleaner.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Azure.Cosmos;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
 
 internal class CosmosDbCleaner
 {
@@ -36,9 +38,38 @@ ORDER BY c.part";
             {
                 var items = await iterator.ReadNextAsync();
                 var itemsByPartition = items
-                    .GroupBy(i => i.Part);
+                    .GroupBy(i => i.Part)
+                    .Select(g => (IImmutableList<PartitionHolder>)g.ToImmutableArray());
+                var stack = new ConcurrentStack<IImmutableList<PartitionHolder>>(itemsByPartition);
+                var deleteTasks = Enumerable.Range(0, Math.Min(stack.Count, _parallelWriters))
+                    .Select(i => DeleteDocumentsAsync(stack))
+                    .ToImmutableArray();
 
-                throw new NotImplementedException();
+                Console.WriteLine($"{items.Count} items getting deleted...");
+                await Task.WhenAll(deleteTasks);
+            }
+        }
+    }
+
+    private async Task DeleteDocumentsAsync(
+        ConcurrentStack<IImmutableList<PartitionHolder>> stack)
+    {
+        while (true)
+        {
+            if (stack.TryPop(out var holder))
+            {
+                var batch =
+                    _container.CreateTransactionalBatch(new PartitionKey(holder.First().Part));
+
+                foreach (var item in holder)
+                {
+                    batch.DeleteItem(item.Id);
+                }
+                await batch.ExecuteAsync();
+            }
+            else
+            {
+                return;
             }
         }
     }
