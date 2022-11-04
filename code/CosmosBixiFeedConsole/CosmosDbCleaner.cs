e78ae90820be657230a91195c0ef8f49c668bd0b
vplauzon <vincent_lauzon@yahoo.com.au>
2022-11-04 16:57:39 -0400

    .

---
+++ code/CosmosBixiFeedConsole/CosmosDbCleaner.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Azure.Cosmos;
+
+internal class CosmosDbCleaner
+{
+    #region Inner Types
+    private class PartitionHolder
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string Part { get; set; } = string.Empty;
+    }
+    #endregion
+
+    private readonly Container _container;
+    private readonly int _parallelWriters;
+
+    public CosmosDbCleaner(Container container, int parallelWriters)
+    {
+        _container = container;
+        _parallelWriters = parallelWriters;
+    }
+
+    public async Task RunAsync()
+    {
+        var queryText = @"
+SELECT c.id, c.part
+FROM c
+ORDER BY c.part";
+
+        while (true)
+        {
+            var iterator = _container.GetItemQueryIterator<PartitionHolder>(queryText);
+            var partitions = new List<string>();
+
+            while (iterator.HasMoreResults)
+            {
+                var items = await iterator.ReadNextAsync();
+                var itemsByPartition = items
+                    .GroupBy(i => i.Part);
+
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
--- code/CosmosBixiFeedConsole/Program.cs
+++ code/CosmosBixiFeedConsole/Program.cs
@@ -14,19 +14,30 @@ if (args.Length < 1)
 else
 {
     var config = await RootConfiguration.LoadConfigAsync(args[0]);
-    var rootDirectoryClient = new DataLakeDirectoryClient(
-        new Uri(config.Storage!.FolderUrl!),
-        new DefaultAzureCredential());
-    var cosmosClient = new CosmosClient(config.CosmosDb!.Endpoint!, config.CosmosDb!.AccessKey!);
+    var cosmosClient =
+        new CosmosClient(config.CosmosDb!.Endpoint!, config.CosmosDb!.AccessKey!);
     var container = cosmosClient
         .GetDatabase(config.CosmosDb!.Database!)
         .GetContainer(config.CosmosDb!.Container);
-    var feeder = new CosmosDbFeeder(
-        rootDirectoryClient,
-        container,
-        config.Storage!.BlobLimit,
-        config.CosmosDb!.ParallelWriters,
-        config.CosmosDb!.BatchSize);
 
-    await feeder.RunAsync();
+    if (args.Length >= 2 && args[1] == "clean")
+    {
+        var cleaner = new CosmosDbCleaner(container, config.CosmosDb!.ParallelWriters);
+    
+        await cleaner.RunAsync();
+    }
+    else
+    {
+        var rootDirectoryClient = new DataLakeDirectoryClient(
+            new Uri(config.Storage!.FolderUrl!),
+            new DefaultAzureCredential());
+        var feeder = new CosmosDbFeeder(
+            rootDirectoryClient,
+            container,
+            config.Storage!.BlobLimit,
+            config.CosmosDb!.ParallelWriters,
+            config.CosmosDb!.BatchSize);
+
+        await feeder.RunAsync();
+    }
 }
